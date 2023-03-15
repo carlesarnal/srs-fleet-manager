ff980b69dbe245ff3f4645053c7fda35e8614567
Carles Arnal <carlesarnal92@gmail.com>
2023-03-15 11:06:01 +0100

    chore: customize auth errors

--- core/src/main/java/org/bf2/srs/fleetmanager/rest/config/AuthenticationFailedExceptionHandler.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/rest/config/AuthenticationFailedExceptionHandler.java
@@ -1,2 +1,25 @@
-package org.bf2.srs.fleetmanager.rest.config;public class AuthenticationFailedExceptionHandler {
+package org.bf2.srs.fleetmanager.rest.config;
+
+import io.quarkus.security.AuthenticationFailedException;
+import io.vertx.ext.web.Router;
+import org.bf2.srs.fleetmanager.rest.privateapi.beans.ErrorInfo1Rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class AuthenticationFailedExceptionHandler {
+
+    public void init(@Observes Router router) {
+        router.route().failureHandler(event -> {
+            if (event.failed() && event.failure() instanceof AuthenticationFailedException) {
+                ErrorInfo1Rest errorInfo = new ErrorInfo1Rest();
+                errorInfo.setErrorCode(event.statusCode());
+                errorInfo.setMessage(event.failure().getMessage());
+                event.response().end(errorInfo.toString());
+            } else {
+                event.next();
+            }
+        });
+    }
 }
